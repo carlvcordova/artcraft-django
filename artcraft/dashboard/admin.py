1ff49bfaccf1979d32d5ead46966c62b8efd64e3
wyntrx <manuelgwyneth97@gmail.com>
2021-09-15 08:36:07 +0800

    Added models

--- artcraft/dashboard/admin.py
+++ artcraft/dashboard/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Artist, Customer, Artwork, Order
 
+admin.site.register(Artist)
+admin.site.register(Customer)
+admin.site.register(Artwork)
+admin.site.register(Order)
 # Register your models here.
