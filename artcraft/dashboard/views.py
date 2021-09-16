ddfe3b5147fbd42a3d2421fe7dd6d8817ffe45b3
wyntrx <manuelgwyneth97@gmail.com>
2021-09-16 08:41:31 +0800

    changes

--- artcraft/dashboard/views.py
+++ artcraft/dashboard/views.py
@@ -6,4 +6,15 @@ from django.views.generic import View
 
 class dashboardView(View):
     def get(self, request):
+<<<<<<< Updated upstream
         return render(request, 'dashboard.html', {})
+=======
+        
+        artist = Artist.objects.all()
+        customer = Customer.objects.all()
+        artwork = Artwork.objects.all()
+        order = Order.objects.all()
+        return render(request, 'dashboard.html', 
+            {'artist':artist, 'customer':customer, 'artwork':artwork, 'order':order})
+
+>>>>>>> Stashed changes
